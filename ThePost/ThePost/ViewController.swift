8acafbd75863cfa4858cf53ca9d11a715fabb555
Jordi Milla Catalán <jb.90@hotmail.es>
2020-12-14 17:12:01 +0100

    Install pods.

---
+++ ThePost/ThePost/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThePost
+//
+//  Created by Jordi Milla on 14/12/2020.
+//  Copyright © 2020 Jordi Milla Catalan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
